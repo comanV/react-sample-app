13c38a23158f9fe9db1440490712e404c80096a4
vlad <ens16477@adobe.com>
2022-07-06 16:46:14 +0300

    add functionality to access in the child the data returned by the parent

--- src/connectToParentFromIframe.js
+++ src/connectToParentFromIframe.js
@@ -6,9 +6,19 @@ import {connectToParent} from "penpal";
             hi(string) {
                 document.body.appendChild(document.createElement("br"));
                 document.body.appendChild(new Text(string));
+            },
+            getValueFromChild() {
+                return "I am returned from a child function"
             }
         }
     });
     const parent = await connection.promise;
     parent.hello("Message printed by child to parent");
+
+    // append text retrieved from a parent method
+    parent.getValueFromParent().then((valueReturnedFromParent) => {
+        document.body.appendChild(document.createElement("br"));
+        document.body.appendChild(new Text(valueReturnedFromParent));
+    })
+
 })();
