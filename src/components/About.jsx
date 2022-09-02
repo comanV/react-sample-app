2202465a6e61f3a305f3a6a0894c89c40161e9b0
vlad <ens16477@adobe.com>
2022-09-02 14:37:38 +0300

    test pre-commit hook2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,12 @@ const About = () => (
     <div>
         <h1>About Us</h1>
         <img width="100%" src={image} alt="Sample" />
-        <p>The WKND is a fictional online magazine and Adventure Company that focuses
+        <p>The WKND is a fictional online magazine and adventure company that focuses
           on outdoor activities and trips across the globe. The WKND site is designed 
           to demonstrate functionality for Adobe Experience Manager. There is also a 
           corresponding tutorial that walks a developer through the development.  
           Special thanks to Lorenzo Buosi and Kilian Amendola who created the 
-          beautiful design for the WKND site.
+          beautiful design for the WKND site. Hello Test.
         </p>
     </div>
 );
