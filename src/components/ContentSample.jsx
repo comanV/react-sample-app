5df94945535516e4f5b635f91d002fa3e2ec510e
svinod <svinod@adobe.com>
2022-10-03 10:42:35 +0200

    fix: update versions

--- src/components/ContentSample.jsx
+++ src/components/ContentSample.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useEffect } from 'react';
+import Loading from './Loading';
 const { REACT_APP_IBIZA_URL, REACT_APP_API_KEY } = process.env;
 
 const ContentSample = () => {
@@ -14,14 +15,14 @@ const ContentSample = () => {
     .then(html => {
       var parser = new DOMParser();
       var doc = parser.parseFromString(html, "text/html");
-      setContent(doc.querySelector('main').innerHTML);
+      setContent(doc.querySelector('body').innerHTML);
     })
     .catch(function(err) {  
         console.log('Failed to fetch page: ', err);  
     });
   }, []);
 
-  return <div className="article" dangerouslySetInnerHTML={{__html:content}}/>;
+  return !content ? <Loading/> : <div className="article" dangerouslySetInnerHTML={{__html:content}}/>;
 };
 
 export default ContentSample;
